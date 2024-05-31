af7da0b021348d5ab4caba9b8c109c0f11b0418e
Becker Jean-Christophe <mothos@hotmail.fr>
2024-05-31 23:19:12 +0200

    first commit

---
+++ src/test/java/fr/mayccos/stock/StockApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.mayccos.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
