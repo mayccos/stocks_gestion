71207e0ccb6b330013e6504b1c19362c3ad1e51a
jean-christophe Becker <mothos@hotmail.fr>
2024-06-08 19:26:20 +0200

    feat(mqtt):implements MqttConfig

--- src/main/java/fr/mayccos/stock/configuration/MqttConfig.java
+++ src/main/java/fr/mayccos/stock/configuration/MqttConfig.java
@@ -1,4 +1,21 @@
 package fr.mayccos.stock.configuration;
 
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MqttConfig {
+
+    @Value("${mqtt.brocker.url}")
+    private String brockerUrl;
+
+    @Value("${mqtt.client.id}")
+    private String clientId;
+
+    @Value("${mqtt.username}")
+    private  String username;
+
+    @Value("${mqtt.password}")
+    private String password;
 }
